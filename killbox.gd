f3f22404ad80d3afe64ca079dc3ddba9c9965cb8
hadinswi <116318212+hadinswi@users.noreply.github.com>
2023-10-20 09:22:06 -0700

    Update killbox.gd

--- killbox.gd
+++ killbox.gd
@@ -1,7 +1,1 @@
-extends Area2D
-
-signal player_reset_requested
-
-func _on_Area2D_body_entered(body):
-	if body.name == "Player":
-		emit_signal("player_reset_requested", body)
+#i got rid of this for the file because it makes the game break at the moment
